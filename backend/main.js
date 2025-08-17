bacb913d24fa8384a34dbe6208668bba7f27a768
cbum-dev <priyanshukumar2002234@gmail.com>
2025-08-17 23:41:41 +0530

    added frontend and backend

---
+++ backend/main.js
@@ -0,0 +1,11 @@
+// import express from "express"
+
+// const app = express();
+
+// app.get("/",(req,res) => {
+//     res.send("hellow world 🧚")
+// })
+
+// app.listen(3000,console.log("hi"))
+
+console.log("eh🧚")
